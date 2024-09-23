5d6f5b045e352787996366f63d2ece7deb516e21
BrianDUPONT <brian-dupont@hotmail.com>
2024-09-23 16:58:58 +0200

    Modification des imports

--- src/main/java/bts/sio/webapp/controller/AthleteController.java
+++ src/main/java/bts/sio/webapp/controller/AthleteController.java
@@ -2,8 +2,10 @@ package bts.sio.webapp.controller;
 
 import bts.sio.webapp.model.Athlete;
 import bts.sio.webapp.model.Pays;
+import bts.sio.webapp.model.Sport;
 import bts.sio.webapp.service.AthleteService;
 import bts.sio.webapp.service.PaysService;
+import bts.sio.webapp.service.SportService;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,6 +26,9 @@ public class AthleteController {
 
     @Autowired
     private PaysService paysService;
+
+    @Autowired
+    private SportService sportService;
 
     @GetMapping("/")
     public String home(Model model) {

6c97976a8c514b28229c811247610b3169cc897a
BrianDUPONT <brian-dupont@hotmail.com>
2024-09-25 09:56:56 +0200

    Ajout d'importations et de private OlympiadeService

--- src/main/java/bts/sio/webapp/controller/AthleteController.java
+++ src/main/java/bts/sio/webapp/controller/AthleteController.java
@@ -3,9 +3,11 @@ package bts.sio.webapp.controller;
 import bts.sio.webapp.model.Athlete;
 import bts.sio.webapp.model.Pays;
 import bts.sio.webapp.model.Sport;
+import bts.sio.webapp.model.Olympiade;
 import bts.sio.webapp.service.AthleteService;
 import bts.sio.webapp.service.PaysService;
 import bts.sio.webapp.service.SportService;
+import bts.sio.webapp.service.OlympiadeService;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -29,6 +31,9 @@ public class AthleteController {
 
     @Autowired
     private SportService sportService;
+
+    @Autowired
+    private SportService olympiadeService;
 
     @GetMapping("/")
     public String home(Model model) {

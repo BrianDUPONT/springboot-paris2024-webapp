bcea09082aa335c93d57d68227f7350dcf92f554
BrianDUPONT <brian-dupont@hotmail.com>
2024-09-25 09:55:48 +0200

    Création du modèle Olympiade

--- src/main/java/bts/sio/webapp/model/Olympiade.java
+++ src/main/java/bts/sio/webapp/model/Olympiade.java
@@ -10,10 +10,8 @@ public class Olympiade {
 
     private Integer id;
     private String numero;
-    private String annee;
+    private Integer annee;
     private String ville ;
     private Pays pays;
-
-
 
 }
